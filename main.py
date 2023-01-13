af12b22c6cd8016cc74e992240d7052d2a4cef44
ioana <ioanaardelean555@gmail.com>
2023-01-13 19:35:06 +0200

    move optimizer in training loop

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from RNN import RNN
 
 def main():
     print("main function run")
-    n_hidden = 50  # hyper parameters
-    learning_rate = 0.005
+    n_hidden = 125  # hyper parameters
+    learning_rate = 0.001
     n_iters = 600
     rnn = trainingLoop(n_hidden, learning_rate,n_iters)
     test_count = 400 #400 is maximum
-    runPerformance(rnn, test_count, "results3.txt", learning_rate, n_hidden, n_iters)
+    runPerformance(rnn, test_count, "results4.txt", learning_rate, n_hidden, n_iters)
 main()
--- training.py
+++ training.py
@@ -4,23 +4,13 @@ from RNN import *
 from frameProcessing import FeedData
 from utils import category_from_output
 # inspiration: https://www.youtube.com/watch?v=WEV61GmmPrk&list=PLiDmKRJhglti6HwdDP9kEItTlHMZCDPk_&index=4&t=184s
-# params from name classification example
+#problems with loss
 
-#problems with loss and epochs, is it going in the whole training set? how many times?
-
-
-
-# def train : one gif aka one training step
-#line_tensor: whole name, for me whole list of frames
-def train(line_tensor, category_tensor, optimizer, rnn, criterion):
+def train(line_tensor, rnn):
     hidden = rnn.init_hidden()
     for i in range(line_tensor.size()[0]):
         output, hidden = rnn(torch.FloatTensor(line_tensor[i]), hidden)
-    loss = criterion(output,category_tensor)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    return output, loss.item()
+    return output
 
 def trainingLoop(n_hidden, learning_rate,n_iters):
     n_input = 1  # size of one seq input in rnn
@@ -29,13 +19,15 @@ def trainingLoop(n_hidden, learning_rate,n_iters):
     rnn = RNN(n_input, n_hidden, n_categories)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
-    current_loss = 0
     for i in range(n_iters):
         print(str(i) + " out of " + str(n_iters))
         category, line, category_tensor, line_tensor = loader.feedNextTrain()
-        output, loss = train(line_tensor, category_tensor, optimizer, rnn, criterion)
-        current_loss += loss
-        print(" loss " + str(current_loss))
+        output = train(line_tensor, rnn)
+        loss = criterion(output, category_tensor)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(" loss " + str(loss.item()))
     return rnn
 
 def manualTest(RNN):
